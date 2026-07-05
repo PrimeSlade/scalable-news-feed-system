7add5e4ea957a52bf62fe854450add628fb6ed0c
Slade <saib6064@gmail.com>
2026-07-05 20:20:22 +0700

    refactor: remove asyncHandler wrapper, Express 5 handles async natively

--- src/modules/feed/feed.routes.ts
+++ src/modules/feed/feed.routes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import * as feedController from "./feed.controller";
-import { asyncHandler } from "../../utils/async-handler";
 
 const router = Router();
 
-router.post("/", asyncHandler(feedController.createPost));
+router.post("/", feedController.createPost);
 
 export default router;
